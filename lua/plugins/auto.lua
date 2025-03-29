bba21bd3b0de728268b30108eb074f46d0dfbc0a
zhaozg <zhaozg@gmail.com>
2025-03-29 15:53:38 +0800

    add set filetype from neoconf

--- lua/plugins/auto.lua
+++ lua/plugins/auto.lua
@@ -1,6 +1,24 @@
 -- 在 LazyVim 的插件配置中：
 return {
   {
+    "folke/neoconf.nvim",
+    cmd = "Neoconf",
+    opts = function(_, opts)
+      local function setup_filetype()
+        local conf = require("neoconf").get("filetype") or {}
+        for k, v in pairs(conf) do
+          vim.api.nvim_create_autocmd({'BufRead', 'BufNewFile'}, {
+            pattern = k,
+            command = "set filetype=" .. v
+          })
+        end
+      end
+      setup_filetype()
+
+      return opts
+    end
+  },
+  {
     "keaising/im-select.nvim",
     config = function()
       require("im_select").setup({
