c5e5289b8c56afc278c2381e7303c6c04a66bac7
zhaozg <zhaozg@gmail.com>
2025-04-19 22:13:49 +0800

    disable many provider

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,3 +2,7 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.g.autoformat = false
+vim.g.loaded_node_provider = false
+vim.g.loaded_perl_provider = false
+vim.g.loaded_ruby_provider = false
+vim.g.loaded_python_provider = false
