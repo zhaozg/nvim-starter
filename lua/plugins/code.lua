9e217b00a8e3061216b7ce68163ed153d623026b
zhaozg <zhaozg@gmail.com>
2025-05-30 21:03:15 +0800

    introduce refactoring.nvim

--- lua/plugins/code.lua
+++ lua/plugins/code.lua
@@ -1,5 +1,14 @@
 return {
   {
+    "ThePrimeagen/refactoring.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    lazy = false,
+    opts = {},
+  },
+  {
     'andythigpen/nvim-coverage',
     after = "nvim-lua/plenary.nvim",
     config = function()
