e369e72db545ca179ccf60fd1b285d904fcf4cba
zhaozg <zhaozg@gmail.com>
2025-06-17 15:10:16 +0800

    improve IME in neovide

--- lua/plugins/neovide.lua
+++ lua/plugins/neovide.lua
@@ -114,4 +114,29 @@ end
 -- system clipboard }}}
 
 vim.api.nvim_set_keymap("n", "<leader>tw", "<cmd>let g:neovide_fullscreen = !g:neovide_fullscreen<cr>", {})
+
+-- {{ IME support for neovide
+local function set_ime(args)
+  if args.event:match("Enter$") then
+    vim.g.neovide_input_ime = true
+  else
+    vim.g.neovide_input_ime = false
+  end
+end
+
+local ime_input = vim.api.nvim_create_augroup("ime_input", { clear = true })
+
+vim.api.nvim_create_autocmd({ "InsertEnter", "InsertLeave" }, {
+  group = ime_input,
+  pattern = "*",
+  callback = set_ime
+})
+
+vim.api.nvim_create_autocmd({ "CmdlineEnter", "CmdlineLeave" }, {
+  group = ime_input,
+  pattern = "[/\\?]",
+  callback = set_ime
+})
+-- }}
+
 return {}
