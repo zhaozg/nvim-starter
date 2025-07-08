44535eccfada7ec79407628c8379828b3d68a92f
zhaozg <zhaozg@gmail.com>
2025-07-08 20:24:44 +0800

    update mini.snippets

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -3,7 +3,6 @@ return {
     "echasnovski/mini.nvim",
     config = function(_, opts)
       require('mini.move').setup()
-      require('mini.snippets').setup()
       require('mini.cursorword').setup()
       require('mini.operators').setup()
       require('mini.sessions').setup()
@@ -16,6 +15,17 @@ return {
         },
       })
 
+      local gen_loader = require('mini.snippets').gen_loader
+      require('mini.snippets').setup({
+        snippets = {
+          -- Load custom file with global snippets first (adjust for Windows)
+          gen_loader.from_file('~/.config/nvim/snippets/package.json'),
+
+          -- Load snippets based on current language by reading files from
+          -- "snippets/" subdirectories from 'runtimepath' directories.
+          gen_loader.from_lang(),
+        },
+      })
 
       require('mini.trailspace').setup()
       -- HACK: We need to disabel the mini.trailspace and enable when a new buffer is
