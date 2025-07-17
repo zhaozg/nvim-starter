a991f04a29a5f5821a310b52c2b7fc55edb16497
zhaozg <zhaozg@gmail.com>
2025-07-17 12:43:54 +0800

    修正mini-snippets设置

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -19,12 +19,13 @@ return {
       require('mini.snippets').setup({
         snippets = {
           -- Load custom file with global snippets first (adjust for Windows)
-          gen_loader.from_file('~/.config/nvim/snippets/package.json'),
+          gen_loader.from_file('~/.config/nvim/snippets/all.json'),
 
           -- Load snippets based on current language by reading files from
           -- "snippets/" subdirectories from 'runtimepath' directories.
           gen_loader.from_lang(),
         },
+        mappings = { stop = "<esc>" }, -- <c-c> by default, see :h MiniSnippets-session
       })
 
       require('mini.trailspace').setup()
